e6b95c85649faf82e5e5e239ebf898db49b375ac
Cameron <mcmillancameron87@gmail.com>
2023-02-06 11:36:46 +0000

    adding

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,10 @@
-const a = 'b';
-console.log('Hello this is mashup js')
+const a = "b";
+console.log("Hello this is mashup js" + a);
+
+const myStatement = false;
 
 if (myStatement = false)
 
 {
-  console.log('This is mashup' + 'strings'); if (true == false) { }
+  console.log("This is mashup" + "strings"); if (true == false) { }
 }
